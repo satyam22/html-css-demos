c6e36e727c96c77e5f72eca71ec3fb283b6700aa
Satyam Bansal <satyamsgsits1994@gmail.com>
2017-12-26 17:27:15 +0530

    travel for gek theme complete

--- public/stylish_portfolio/js/stylish-portfolio1.js
+++ public/stylish_portfolio/js/stylish-portfolio1.js
@@ -4,5 +4,27 @@
         $('#sidebar-wrapper').toggleClass('active');
         $('.menu-toggle > .fa-bars,.menu-toggle > .fa-times').toggleClass('fa-bars fa-times');
         $(this).toggleClass('active');
+    });
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(){
+        if(location.pathname.replace(/^\//,'')===this.pathname.replace(/^\//,'')&&location.hostname===this.hostname){
+            var target=$(this.hash);
+            target=target.length?target:$('[name='+this.hash.slice(1)+']');
+            if(target.length){
+                $('html,body').animate({
+                    scrollTop:target.offset().top
+                
+                },1000,"easeInOutExpo");
+                return false;
+            }
+        }
+    })
+    $(document).scroll(function(){
+        var scrollDistance=$(this).scrollTop();
+        if(scrollDistance>100){
+            $('.scroll-to-top').fadeIn();
+        }
+        else{
+            $('.scroll-to-top').fadeOut();
+        }
     })
 })(jQuery)
